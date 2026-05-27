67c07cddb97479e7367dec3cad99b5f97a4d726e
ML-Allie <allie.wh@medialand.tw>
2026-05-27 15:29:37 +0800

    feat: 調整store/main.js

--- src/store/main.js
+++ src/store/main.js
@@ -1,25 +1,41 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useMainStore = defineStore('main', {
-  state: () => ({
-    isLoading: true,
-    popup: null,
-    popupQueue: null,
-  }),
-  actions: {
-    increment() {
-      this.count++;
-    },
-    setLoading(boolean) {
-      this.$patch((state) => {
-        state.isLoading = boolean;
-      });
-    },
-    openPopup(type) {
-      this.popup = type;
-    },
-    closePopup() {
-      this.popup = null;
-    },
-  },
-});
+export const useMainStore = defineStore('main', () => {
+  // State
+  const isLoading = ref(true)
+  const isMobile = ref(null)
+  const popup = ref(null)
+  const popupQueue = ref(null)
+
+  // Actions
+  const increment = () => {
+    count.value++
+  }
+
+  const setLoading = (boolean) => {
+    isLoading.value = boolean
+  }
+  const setIsMobile = (boolean) => {
+    isMobile.value = boolean
+  }
+  const openPopup = (type) => {
+    popup.value = type
+  }
+  const closePopup = () => {
+    popup.value = null
+  }
+
+
+  return {
+    isLoading,
+    isMobile,
+    popup,
+    popupQueue,
+    increment,
+    setLoading,
+    setIsMobile,
+    openPopup,
+    closePopup,
+  }
+})
